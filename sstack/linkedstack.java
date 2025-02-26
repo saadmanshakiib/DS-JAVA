55753876a5a18b59c7470d502b1287467d15724e
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-02-26 18:19:46 +0600

    Add files via upload

---
+++ sstack/linkedstack.java
@@ -0,0 +1,23 @@
+package sstack;
+
+import javax.sound.sampled.Line;
+import java.util.*;
+public class linkedstack<E> implements Stack<E> {
+    private LinkedList<E> list = new LinkedList<E>();
+    private LinkedList<E> ;
+
+    public linkedstack(){}
+    public int size(){ return list.size();}
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+    public void push(E e){
+        list.addFirst(e);
+    }
+    public E pop(){
+      return list.removeFirst();
+    }
+    public E top(){
+        return list.getFirst();
+    }
+}
---
+++ sstack/stack.java
@@ -0,0 +1,37 @@
+package sstack;
+
+import java.util.Stack;
+
+public class stack<E> implements Stack<E> {
+    public static final int capacity = 1000;
+    private E[] array;
+    private int top = -1;
+    public stack(){
+        this(capacity);
+    }
+    public stack(int capacity){
+        array = (E[]) new Object[capacity];
+    }
+    public int size(){
+        return top+1;
+    }
+    public boolean isEmpty(){
+        return top==-1;
+    }
+    public void push(E e) throws IllegalStateException{
+        if(size()==array.length) throw new IllegalStateException("Stack overflow");
+        array[++top] = e;
+        System.out.println("Pushed");
+    }
+    public E top(){
+        if(isEmpty()) return null;
+        return array[top];
+    }
+    public E pop(){
+        if(isEmpty()) return null;
+        E answer = array[top];
+        array[top] = null;
+        top--;
+        return answer;
+    }
+}
